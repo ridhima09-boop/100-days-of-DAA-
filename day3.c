dd5cddabe7c4603e01c4ffb577315ebad8ac8351
ridhima09-boop <ridhimasuneja9@gmail.com>
2026-09-21 21:37:13 +0530

    Update day3.c

--- day3.c
+++ day3.c
@@ -1,20 +1,33 @@
-def longest_increasing_streak(temperatures):
-    if not temperatures:
-        return 0
+#include <stdio.h>
 
-    max_streak = 1
-    current_streak = 1
+int longest_increasing_streak(int temperatures[], int n) {
+    if (n == 0)
+        return 0;
 
-    for i in range(1, len(temperatures)):
-        if temperatures[i] > temperatures[i - 1]:
-            current_streak += 1
-            max_streak = max(max_streak, current_streak)
-        else:
-            current_streak = 1
+    int max_streak = 1;
+    int current_streak = 1;
 
-    return max_streak
+    for (int i = 1; i < n; i++) {
+        if (temperatures[i] > temperatures[i - 1]) {
+            current_streak++;
 
+            if (current_streak > max_streak)
+                max_streak = current_streak;
+        }
+        else {
+            current_streak = 1;
+        }
+    }
 
-# Example Usage
-temperatures = [10, 12, 15, 14, 16, 18, 20]
-print(longest_increasing_streak(temperatures))  # Output: 4
+    return max_streak;
+}
+
+int main() {
+    int temperatures[] = {10, 12, 15, 14, 16, 18, 20};
+    int n = sizeof(temperatures) / sizeof(temperatures[0]);
+
+    printf("Longest increasing streak: %d\n",
+           longest_increasing_streak(temperatures, n));
+
+    return 0;
+}
