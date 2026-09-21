004eb1486d0d27fe973cfb37bd238c02485c2681
ridhima09-boop <ridhimasuneja9@gmail.com>
2026-09-21 15:28:09 +0530

    Add password prompt and validation in day2.c

---
+++ day2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char password[20];
+
+    while (1)
+    {
+        printf("Enter password: ");
+        scanf("%s", password);
+
+        if (strcmp(password, "12345") == 0)
+        {
+            break;
+        }
+
+        printf("Incorrect password. Try again.\n");
+    }
+
+    printf("Login successful!");
+
+    return 0;
+}
